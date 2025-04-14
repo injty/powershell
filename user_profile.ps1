43d4de1771ce3afa96f3c4da0e379783d322465b
injty <ibrahimovofficial@gmail.com>
2025-04-14 22:38:13 +0400

    chore(settings): added default settings, fonts, ligatures and schemes

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,35 +1,35 @@
 # Prompt
 # Install-Module posh-git -Scope CurrentUser -Force
 # Install-Module oh-my-posh -Scope CurrentUser -Force
-Import-Module posh-git
+# Import-Module posh-git
 
 # Icons
 # Install-Module -Name Terminal-Icons -Repository PSGallery -Force
-Import-Module Terminal-Icons
+# Import-Module Terminal-Icons
 
 # z
 # Install-Module -Name z -Force
 Import-Module Z
 
 # PSReadLine
-# Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
-Import-Module PSReadLine
-Set-PSReadLineOption -BellStyle None
-Set-PSReadLineKeyHandler -Chord "Ctrl+d" -Function DeleteChar
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle InlineView
+# Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+# Import-Module PSReadLine
+# Set-PSReadLineOption -BellStyle None
+# Set-PSReadLineKeyHandler -Chord "Ctrl+d" -Function DeleteChar
+# Set-PSReadLineOption -PredictionSource History
+# Set-PSReadLineOption -PredictionViewStyle InlineView
 
 # scoop install fzf
 # Install-Module -Name PSFzf -Scope CurrentUser -Force
-Import-Module PSFzf
-Set-PSFzfOption -PSReadLineChordProvider "Ctrl+f" -PSReadLineChordReverseHistory "Ctrl+r"
+# Import-Module PSFzf
+# Set-PSFzfOption -PSReadLineChordProvider "Ctrl+f" -PSReadLineChordReverseHistory "Ctrl+r"
 
 # Theme
 # create on ~/.config/powershell nova.omp.json file
 # add inside theme settings
-function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
-$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) "nova.omp.json"
-oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+# function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+# $PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) "nova.omp.json"
+# oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
 
 # Utilities
 function which ($command) {
